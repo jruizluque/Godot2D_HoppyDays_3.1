3aebbb901c6edc9fcbaaa1b357f706f5fb082795
Joaquin Ruiz Luque <joaquinruizluque@gmail.com>
2019-07-16 00:12:21 -0300

    Make Bunny Jump

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,13 +1,33 @@
 extends KinematicBody2D
 
-var motion = Vector2(0, 0)
-const SPEED = 500
+var motion = Vector2(0,0)
+
+const SPEED = 1000
+const GRAVITY = 300
+const UP = Vector2(0,-1)
+const JUMP_SPEED = 3000
 
 func _physics_process(delta):
+	
+	apply_gravity()
+	jump()
+	move()
+	move_and_slide(motion, UP)
+
+func apply_gravity():
+	if is_on_floor():
+		motion.y = 0
+	else:
+		motion.y += GRAVITY
+
+func jump():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		motion.y -= JUMP_SPEED
+
+func move():
 	if Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
 		motion.x = -SPEED
 	elif Input.is_action_pressed("right") and not Input.is_action_pressed("left"):
 		motion.x = SPEED
 	else:
 		motion.x = 0
-	move_and_slide(motion)
