6a1043d79e865c838ec7e881dcabdf1790139259
Joaquin Ruiz Luque <joaquinruizluque@gmail.com>
2019-07-23 00:19:46 -0300

    AnimatedSprite - I Got The Moves Like Bunny

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,16 +2,17 @@ extends KinematicBody2D
 
 var motion = Vector2(0,0)
 
-const SPEED = 1000
+const SPEED = 1500
 const GRAVITY = 300
 const UP = Vector2(0,-1)
-const JUMP_SPEED = 3000
+const JUMP_SPEED = 5000
 
 func _physics_process(delta):
 	
 	apply_gravity()
 	jump()
 	move()
+	animate()
 	move_and_slide(motion, UP)
 
 func apply_gravity():
@@ -21,7 +22,7 @@ func apply_gravity():
 		motion.y += GRAVITY
 
 func jump():
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_pressed("jump") and is_on_floor():
 		motion.y -= JUMP_SPEED
 
 func move():
@@ -31,3 +32,22 @@ func move():
 		motion.x = SPEED
 	else:
 		motion.x = 0
+
+func animate():
+	if motion.y < 0:
+		$AnimatedSprite.play("jump")
+	elif motion.x != 0:
+		$AnimatedSprite.play("walk")
+		if motion.x < 0:
+			$AnimatedSprite.flip_h = true
+		else:
+			$AnimatedSprite.flip_h = false
+	else:
+		$AnimatedSprite.play("idle")
+	
+	
+	
+	
+	
+	
+	
